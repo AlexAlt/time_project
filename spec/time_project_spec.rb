7ca33b660eec89a4a5bd03edc687fea4fdd17846
Alex M Altieri & Rachael A Berecka <student@epicodus.com>
2015-08-04 11:15:58 -0700

    make specs clearer

--- spec/time_project_spec.rb
+++ spec/time_project_spec.rb
@@ -2,11 +2,11 @@ require('rspec')
 require('time_project.rb')
 
 describe('Time#day_of_week') do
-  it("says get up") do
+  it("if it is a work day says get up") do
     expect(Time.new(2015, 8, 3).day_of_week()).to(eq("Get up."))
   end
 
-  it ("say sleep in") do
+  it ("if it is a saturday or a sunday say sleep in") do
     expect(Time.new(2015, 8, 1).day_of_week()).to(eq("Sleep in."))
   end
 end
